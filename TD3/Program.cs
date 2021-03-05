b3f8b3dd3df9ab9bf7723c5d3c520094a3171418
GuillaumeBG <guillaume.bourg24@gmail.com>
2021-03-05 21:12:28 +0100

    modification menu

--- TD3/Program.cs
+++ TD3/Program.cs
@@ -22,14 +22,17 @@ namespace TD3
             MyImage image = new MyImage(nom1);
             bool fin =true;
             bool afficher=false;
+            bool menu_valide = true;
+
             while(fin==true)
             {
+                menu_valide = true;
                 Console.WriteLine("Rentrez le nombre corespondant à l'action que vous voulez réaliser :\n" +
                     "1) Transformez votre image en Nuance De Gris\n" +
                     "2) Transformez votre image en Noir et Blanc\n" +
                     "3) Appliquez un effet miroir à votre image\n" +
                     "4) Appliquer une rotation à l'image\n" +
-                    "5) Detection de contour\n");
+                    "5) Filtre\n");
                 int n=Convert.ToInt32(Console.ReadLine());
                 switch (n)
                 {
@@ -38,17 +41,6 @@ namespace TD3
                         Console.WriteLine("Nous allons modifier votre image en nuance de gris :");
                         NuanceDeGris(image);
                         Console.Clear();
-                        Console.WriteLine("Votre image a été modifiée, souhaitez vous l'afficher ?(1 pour oui et 0 pour non)");
-                        afficher = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
-                        if (afficher==true)
-                        {
-                            image.From_Image_To_File("./temp.bmp");
-                            Process.Start("temp.bmp");
-                            Console.WriteLine("Appuyer sur une touche pour avancer.");
-                            Console.ReadKey();
-                            File.Delete("./temp.bmp");
-                            afficher=false;
-                        }
                         break;
                     case 2 :
                         Console.Clear();
@@ -63,70 +55,77 @@ namespace TD3
                         }
                         else NoirEtBlanc(image);
                         Console.Clear();
-                        Console.WriteLine("Votre image a été modifiée, souhaitez vous l'afficher ?(1 pour oui et 0 pour non)");
-                        afficher = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
-                        if (afficher==true)
-                        {
-                            image.From_Image_To_File("./temp.bmp");
-                            Process.Start("temp.bmp");
-                            Console.WriteLine("Appuyer sur une touche pour avancer.");
-                            Console.ReadKey();
-                            File.Delete("./temp.bmp");
-                            afficher=false;
-                        }
                         break;
                     case 3 :
                         Console.Clear();
                         Console.WriteLine("Nous allons appliquez un effet miroir à votre image :");
                         Miroir(image);
                         Console.Clear();
-                        Console.WriteLine("Votre image a été modifiée, souhaitez vous l'afficher ?(1 pour oui et 0 pour non)");
-                        afficher = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
-                        if (afficher==true)
-                        {
-                            image.From_Image_To_File("./temp.bmp");
-                            Process.Start("temp.bmp");
-                            Console.WriteLine("Appuyer sur une touche pour avancer.");
-                            Console.ReadKey();
-                            File.Delete("./temp.bmp");
-                            afficher=false;
-                        }
                         break;
                     case 4:
                         Console.Clear();
-                        Console.WriteLine("Nous allons faire tourner l'image selon un angle compris entre 0 et 360 :");
-                        int angle = Convert.ToInt32(Console.ReadLine());
-                        Rotation(image,angle);
+                        Console.WriteLine("********\nCette fonctionnalité n'est pas encore fonctionelle\n********");
+                        menu_valide = false;
+                        Console.ReadLine();
+                        //int angle = Convert.ToInt32(Console.ReadLine());
+                        //Rotation(image,angle);
                         Console.Clear();
-                        Console.WriteLine("Votre image a été modifiée, souhaitez vous l'afficher ?(1 pour oui et 0 pour non)");
-                        afficher = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
-                        if (afficher==true)
-                        {
-                            image.From_Image_To_File("./temp.bmp");
-                            Process.Start("temp.bmp");
-                            Console.WriteLine("Appuyer sur une touche pour avancer.");
-                            Console.ReadKey();
-                            File.Delete("./temp.bmp");
-                            afficher=false;
-                        }
                         break;
                     case 5:
-                        Console.Clear();
-                        Console.WriteLine("Nous allons appliquez un filtre pour faire ressortir les countours de l'image :");
-                        image = Filtre.Convolution(image,3);
-                        Console.Clear();
-                        Console.WriteLine("Votre image a été modifiée, souhaitez vous l'afficher ?(1 pour oui et 0 pour non)");
-                        afficher = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
-                        if (afficher==true)
+                        Console.WriteLine("Rentrez le nombre corespondant au filtre que vous voulez appliquer :\n" +
+                            "1) Detection de contour\n" +
+                            "2) Renforcement des bords\n" +
+                            "3) Flou\n" +
+                            "4) Repoussage\n");
+
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        switch (a)
                         {
-                            image.From_Image_To_File("./temp.bmp");
-                            Process.Start("temp.bmp");
-                            Console.WriteLine("Appuyer sur une touche pour avancer.");
-                            Console.ReadKey();
-                            File.Delete("./temp.bmp");
-                            afficher=false;
+                            case 1:
+                                Console.WriteLine("Nous allons appliquez un filtre pour faire ressortir les countours de l'image :");
+                                image = Filtre.Convolution(image, 1);
+                                Console.Clear();
+                                break;
+                            case 2:
+                                Console.WriteLine("Nous allons appliquez un filtre pour renforcer les bords de l'image :");
+                                image = Filtre.Convolution(image, 2);
+                                Console.Clear();
+                                break;
+                            case 3:
+                                Console.WriteLine("Nous allons appliquez un flou à l'image:");
+                                image = Filtre.Convolution(image, 3);
+                                Console.Clear();
+                                break;
+                            case 4:
+                                Console.WriteLine("Nous allons appliquez repoussage à l'image (pour faire apparaitre un relief):");
+                                image = Filtre.Convolution(image, 4);
+                                Console.Clear();
+                                break;
+                            default:
+                                menu_valide = false;
+                                Console.WriteLine("Le chiffre choisi ne fait pas partie du menu");
+                                break;
                         }
                         break;
+                    default:
+                        menu_valide = false;
+                        Console.WriteLine("Le chiffre choisi ne fait pas partie du menu");
+                        break;
+                }
+
+                if (menu_valide)
+                {
+                    Console.WriteLine("Votre image a été modifiée, souhaitez vous l'afficher ?(1 pour oui et 0 pour non)");
+                    afficher = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+                    if (afficher == true)
+                    {
+                        image.From_Image_To_File("./temp.bmp");
+                        Process.Start("temp.bmp");
+                        Console.WriteLine("Appuyer sur une touche pour avancer.");
+                        Console.ReadKey();
+                        File.Delete("./temp.bmp");
+                        afficher = false;
+                    }
                 }
                 Console.WriteLine("Souhaitez-vous continuer à modifier votre image ? (1 pour oui et 0 pour non)");
                 fin = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
@@ -150,7 +149,7 @@ namespace TD3
                     matriceBGR[i,j].R=moyenne;
                     matriceBGR[i,j].V=moyenne;
                     matriceBGR[i,j].B=moyenne;
-                    moyenne =0;
+                    moyenne = 0;
                 }
             }
             image.MatriceBGR = matriceBGR;
